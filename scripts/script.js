6af1c6d7bc6ea2af909ced547d1f9a249aecc65f
AzathothJr <saaim1998@gmail.com>
2026-03-16 22:52:36 +0530

    Added feature products on home page dynamically via faker api

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
-const featuredProductsContainer = document.getElementById("featured-products");
+const featuredProductsContainer = document.querySelector(".featured-products");
 
 fetch('https://fakestoreapi.com/products/').then((data) => {
-    console.log(data);
+    return data.json();
+})
+.then((products) => {
+    // console.log(products);
+    let htmlContent = "";
+    products.forEach((product) => {
+        htmlContent += `
+            <div class="featured-product scale">
+                <figure>
+                    <img src="${product.image}" alt="${product.title}" class="featured-product-image">
+                    <figcaption class="featured-image-text">${product.title}</figcaption>
+                </figure>
+                <p class="featured-product-price">$${product.price}</p>
+            </div>
+        `;
+    });
+    featuredProductsContainer.insertAdjacentHTML('beforeend', htmlContent);
 });
